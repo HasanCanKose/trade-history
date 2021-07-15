cc2935c05640e3ba18f75ea326b7c1b840605b18
Hasan Can Kose <hasancannkosee@gmail.com>
2021-07-15 03:13:21 +0300

    Add navbar and its style

---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = ({ title }) => {
+  return (
+    <div className="navbar bg-primary">
+      <h2>{title}</h2>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/chart">Chart</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: "Trade History App",
+};
+export default Navbar;
