77380e62552aba2cdab6e3fd12011a8d03e9d599
Hasan Can Kose <hasancannkosee@gmail.com>
2021-07-16 14:18:46 +0300

    Add all charts with their values

--- src/components/chart/BarChart.jsx
+++ src/components/chart/BarChart.jsx
@@ -1,31 +1,17 @@
 import { Bar } from "react-chartjs-2";
 
-const BarChart = () => {
+const BarChart = ({ dates, values, backgroundColor, borderColor, label }) => {
   return (
     <div>
       <Bar
         data={{
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: dates,
           datasets: [
             {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
+              label: label,
+              data: values,
+              backgroundColor: backgroundColor,
+              borderColor: borderColor,
               borderWidth: 1,
             },
           ],
--- src/components/pages/charts/Charts.jsx
+++ src/components/pages/charts/Charts.jsx
@@ -1,9 +1,42 @@
+import { useContext } from "react";
+import TradeHistoryContext from "../../../context/trade-history/tradeHistoryContext";
 import BarChart from "../../chart/BarChart";
 
 const Charts = () => {
+  const { conract, dates } = useContext(TradeHistoryContext);
   return (
     <div>
-      <BarChart />
+      <div>
+        <h2>Toplam Miktar</h2>
+        <BarChart
+          dates={dates}
+          values={conract.map((value) => value.quantity)}
+          backgroundColor={"rgba(153, 102, 255, 0.2"}
+          borderColor={"rgba(153, 102, 255, 1"}
+          label={"Toplam Miktar"}
+        />
+      </div>
+      <div>
+        <h2>Toplam Fiyat</h2>
+        <BarChart
+          dates={dates}
+          values={conract.map((value) => value.price)}
+          backgroundColor={"rgba(54, 162, 235, 0.2)"}
+          borderColor={"rgba(54, 162, 235, 1)"}
+          label={"Toplam Fiyat"}
+        />
+      </div>
+
+      <div>
+        <h2>Ağırlıklı Ortalama Fiyat</h2>
+        <BarChart
+          dates={dates}
+          values={conract.map((value) => value.weightedAveragePrice)}
+          backgroundColor={"rgba(75, 192, 192, 0.2)"}
+          borderColor={"rgba(75, 192, 192, 1)"}
+          label={"Ağırlıklı Ortalama Fiyat"}
+        />
+      </div>
     </div>
   );
 };
